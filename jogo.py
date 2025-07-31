fcfbf6156efee9a93e2a6d576fea351d8291e38b
FabioMatiasOfficial <Fabio.casco.aquino@escola.pr.gov.br>
2025-07-31 12:18:11 +0000

    primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,11 @@
+import randow
+print("Jogo De Fabio_MatiasOfficial")
+print("       ─▄▀─▄▀     🦊        ")
+print("       ──▀──▀     🦊        ")
+print("       █▀▀▀▀▀█▄   🦊        ")
+print("       █░░░░░█─█  🦊        ")
+print("       ▀▄▄▄▄▄▀▀   🦊        ")
+
+numeroSecreto= randow.randrange(0,100)
+totalTentativas = 0
+pontos = 100
