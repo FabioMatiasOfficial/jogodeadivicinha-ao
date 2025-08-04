e729bb95cd4abc833800d1aec95ec028e4d88a39
FabioMatiasOfficial <Fabio.casco.aquino@escola.pr.gov.br>
2025-08-04 13:53:47 +0000

    aula3

--- jogo.py
+++ jogo.py
@@ -5,7 +5,34 @@ print("       ──▀──▀     🦊        ")
 print("       █▀▀▀▀▀█▄   🦊        ")
 print("       █░░░░░█─█  🦊        ")
 print("       ▀▄▄▄▄▄▀▀   🦊        ")
+print("    Jogo de adivinhação     ")
 
 numeroSecreto= randow.randrange(0,100)
 totalTentativas = 0
 pontos = 100
+print("Qual niveis de dificuldade?  ")
+print("(1) - facil (2) - medio (3) - dificil ")
+
+nivel = int(imput("Escolha um nivel"))
+
+if(nivel == 1):
+    print("seu patinho ")
+    totaltentativas = 20
+if(nivel == 2):
+    print("fraco")
+    totaltentativas = 10
+if(nivel == 3):
+    print("konan")
+    totaltentativas = 5
+
+for rodada in range (1, totalTentativas +1):
+    print("Tentativa {} de {}".format(rodada,totalTentativas) )
+    chute_str = input("digite um número entre 1 e 100: ")
+    chute = int(chute_str)
+    if(chute <1 or >100):
+        print("Número invalido")
+        continue
+
+    acertou = chute == numeroSecreto
+    maior = chute == numeroSecreto
+    menor = chute == numeroSecreto
